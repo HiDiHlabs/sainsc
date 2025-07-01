d593d119c473ba3e9d0e98db8f66aedcf24e9db7
niklasmueboe <sputnik.mb@web.de>
2025-07-01 12:16:23 +0200

    add zarrstore file

---
+++ src/zarrstore.rs
@@ -0,0 +1,101 @@
+use ndarray::{Array2, Array3, NdFloat};
+use num::zero;
+use serde_json::Map;
+use std::{error::Error, path::PathBuf, sync::Arc};
+use zarrs::{
+    array::{codec::GzipCodec, Array as ZarrArray, ArrayBuilder, DataType, Element, FillValue},
+    filesystem::FilesystemStore,
+    group::{Group, GroupBuilder, GroupMetadataV3},
+};
+
+const CT_PATH_PREFIX: &str = "/cosine";
+const KDE_PATH: &str = "/kde";
+
+pub struct ZarrChunkInfo {
+    pub store: Arc<FilesystemStore>,
+    pub celltypes: Vec<String>, // TODO: improve
+    pub chunk_idx: Vec<u64>,
+}
+
+pub fn initialize_cosine_zarrstore(
+    path: PathBuf,
+    celltypes: &[String],
+    shape: (usize, usize),
+    chunk_size: (usize, usize),
+    n_chunks: (usize, usize),
+) -> Result<Arc<FilesystemStore>, Box<dyn Error + Send + Sync>> {
+    let store = Arc::new(FilesystemStore::new(path)?);
+
+    let shape = vec![shape.0 as u64, shape.1 as u64];
+    let chunk_shape = vec![chunk_size.0 as u64, chunk_size.1 as u64];
+
+    // generate root
+    let mut root_meta = Map::new();
+    root_meta.insert("shape".into(), serde_json::to_value(shape.clone())?);
+    root_meta.insert("celltypes".into(), serde_json::to_value(celltypes)?);
+    root_meta.insert("chunk_size".into(), serde_json::to_value(chunk_size)?);
+    root_meta.insert("n_chunks".into(), serde_json::to_value(n_chunks)?);
+
+    Group::new_with_metadata(
+        store.clone(),
+        "/",
+        GroupMetadataV3::new().with_attributes(root_meta).into(),
+    )?
+    .store_metadata()?;
+
+    // generate cosine group
+    GroupBuilder::new()
+        .build(store.clone(), CT_PATH_PREFIX)?
+        .store_metadata()?;
+
+    let mut array_builder = ArrayBuilder::new(
+        shape,
+        DataType::Float32,
+        chunk_shape.try_into()?,
+        FillValue::from(0),
+    );
+    array_builder
+        .bytes_to_bytes_codecs(vec![Arc::new(GzipCodec::new(5)?)])
+        .dimension_names(["x", "y"].into());
+
+    // generate empty arrays for celltypes
+    for ct in celltypes {
+        let ct_array = array_builder.build(store.clone(), &format!("{CT_PATH_PREFIX}/{ct}"))?;
+        ct_array.store_metadata()?;
+    }
+
+    // generate empty array for kde
+    let kde_array = array_builder.build(store.clone(), KDE_PATH)?;
+    kde_array.store_metadata()?;
+
+    Ok(store)
+}
+
+pub fn write_cosine_to_zarr<T: NdFloat + Element>(
+    zarr_store: Arc<FilesystemStore>,
+    cosine: &Array3<T>,
+    kde_norm: &Array2<T>,
+    celltypes: &[String],
+    chunk_idx: &[u64],
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    // for store_chunk_ndarray the array must be the exact size of the chunk,
+    // however with a RegularChunkGrid even the dangling chunks (which might be smaller)
+    // have the exact same size
+    // therefore using store_chunk_subset_ndarray
+
+    for (cos, ct) in cosine.outer_iter().zip(celltypes) {
+        let mut cos_norm = &cos / kde_norm;
+        cos_norm.mapv_inplace(|v| if v.is_nan() { zero() } else { v });
+
+        ZarrArray::open(zarr_store.clone(), &format!("{CT_PATH_PREFIX}/{ct}"))?
+            .store_chunk_subset_ndarray(chunk_idx, &[0, 0], cos_norm)?;
+    }
+
+    ZarrArray::open(zarr_store.clone(), KDE_PATH)?.store_chunk_subset_ndarray(
+        chunk_idx,
+        &[0, 0],
+        kde_norm.to_owned(),
+    )?;
+
+    Ok(())
+}
